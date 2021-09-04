6eef30b5d09099c00eaa589c543bdd396ee77146
Ryan Rodriguez <88012366+rr1485@users.noreply.github.com>
2021-09-04 16:07:33 -0400

    Add tutorial file
    This tutorial was thanks to Udemy.com where I took a crash course in Python and learned TensorFlow, NumPy, Panda, and Machine Learning using Python.

---
+++ main.py
@@ -0,0 +1,44 @@
+
+import numpy as np
+
+# print(np.zeros(10))
+#
+# print(np.ones(10))
+#
+# fives = np.arange(0,10)
+# fives[:] = 5
+# print(fives)
+#
+# print(np.arange(10,51))
+#
+# print(np.arange(10,51,2))
+#
+# print(np.arange(0,9).reshape(3,3))
+#
+# print(np.eye(3))
+#
+# print(np.random.rand(1))
+#
+# print(np.random.randn(25))
+
+# print(np.arange(0.01,1.01,.01).reshape((10,10)))
+
+# print(np.linspace(0,1,20))
+
+# mat = np.arange(1,26).reshape(5,5)
+# print(mat[2:,1:])
+
+# print(mat[3][4])
+
+# print(mat[:3,1:2])
+
+# print(mat[4:,:])
+
+# print(mat[3:,:])
+
+# print(np.sum(mat[:]))
+
+# print(np.std(mat[:]))
+
+# print(sum(mat[:,:]))
+
